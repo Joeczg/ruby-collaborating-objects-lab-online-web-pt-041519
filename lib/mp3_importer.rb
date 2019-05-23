f9081aac18d766976b6bfc43f76a82c79a9f8c14
zhenggao cai <caizhengao1992@gmail.com>
2019-05-23 17:54:15 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,7 +4,12 @@ class MP3Importer
     @path = path
     @file_names = []
   end
-  
+  def files
+    Dir.entries(path).each do |filename|
+      @filenames << "#{filename}"
+    end
+    @filenames.delete_if {|x| x == "." || x == ".."}
+  end
   def import(list_of_file_name)
     
     
