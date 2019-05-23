6eef3757dc03fa73e89bf32096a4cb1bf08450b0
zhenggao cai <caizhengao1992@gmail.com>
2019-05-23 17:52:41 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,11 @@
 class MP3Importer
   attr_accessor :path , :file_names
-  def initialize
-    @path = 
+  def initialize(path)
+    @path = path
+    @file_names = []
+  end
+  
   def import(list_of_file_name)
+    
+    
     
