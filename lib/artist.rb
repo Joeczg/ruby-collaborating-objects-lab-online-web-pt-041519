9376613be8103fb348fcaae403658d88b59fb1bf
zhenggao cai <caizhengao1992@gmail.com>
2019-05-23 18:05:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+   all created instances of Artist class in 
+                      #@@all class variable
+  end
+attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song    #Artist.songs reader returns all songs 
+                          #stored in @songs
+  end
+
+  def save
+    @@all << self     #saves
+  def self.all      #class method all returns all instances of Artist class 
+    @@all
+  end
